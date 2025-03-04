3e7efc5615afd327b2a3e5911741d2374fa739ce
bon <kaste2233@gmail.com>
2025-03-04 17:14:03 +0900

    Add : Editor컴포넌트 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,37 @@
+'use client';
+
+import { useState } from 'react';
+import Editor from '../components/Editor';
+import { saveBase64Image } from '../utils/image.utils';
+
 export default function Home() {
+  const [imgFile, setImgFile] = useState<File | null>(null);
+
+  const handleSubmit = (dataUrl: string) => {
+    setImgFile(null);
+    saveBase64Image(dataUrl);
+  };
+
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
-        home
+    <div className='min-h-screen p-8 pb-20 gap-16'>
+      <main>
+        <label className='flex flex-col gap-4 cursor-pointer'>
+          이미지 첨부하기
+          <input
+            type='file'
+            accept='image/*'
+            className='p-3 rounded-lg border-[1px]'
+            onChange={(e) => {
+              const file = e?.target?.files?.[0];
+              if (file) {
+                setImgFile(file);
+              }
+            }}
+          />
+        </label>
+        <div className='mt-4 flex flex-col gap-4 row-start-2 items-center sm:items-start'>
+          {imgFile && <Editor bgImage={imgFile} onSubmit={handleSubmit} />}
+        </div>
       </main>
     </div>
   );
